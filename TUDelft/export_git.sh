4c9677893929ef6619704f5b7ec7bfbf6bca3f50
Christophe De Wagter <dewagter@gmail.com>
2010-12-07 20:19:38 +0100

    import/export scripts

--- TUDelft/export_git.sh
+++
@@ -1,7 +0,0 @@
-cd ..
-git clone git://github.com/paparazzi/paparazzi.git ./readonlypprz
-cd ./readonlypprz
-git pull origin
-cd ..
-cp -fR ./readonlypprz/*  #1
-
--- TUDelft/import.sh
+++ TUDelft/import.sh
@@ -1,43 +1,50 @@
-echo "Warning: Did you commit first?"
-echo "Updating TUDelft Paparazzi Version"
-svn update ./
+echo "Updating/Cleaning SVN"
 rm -vrf ./conf/conf.xml.*-*
-echo "Cleaning and Updating ENAC Paparazzi Version"
-cd ../contribute2pprz/
-git reset --hard 
+echo "Make Clean"
+make clean 2&>1 | grep 2139842398470129387401283947
+echo "Rebase and Dcommit"
+git svn rebase
+git svn dcommit
+echo "GIT VERSION"
+cd ../tudelft/
+git checkout master
+git reset --hard
+echo "Remove Untracked Files" 
 git status -s | sed 's/??/rm -rf/' > rm.sh
 chmod +x ./rm.sh
 ./rm.sh
-git fetch paparazzi
-git pull paparazzi master
-echo "Exporting ENAC to TUDelft Paparazzi Version"
-git archive master | tar -x -C ../pprz3svn
-cd ../ppz3svn/
+echo "Pull latest remote version"
+git fetch tudelft
+git pull tudelft master
+echo "Exporting GIT -> SVN"
+git archive master | tar -x -C ../gitsvnpprz
+cd ../gitsvnpprz/
 echo "Adding new Files"
-svn status | grep '? ' | sed 's/?      /svn add/' > add.sh
+git status -s | grep -v add.sh | grep '?? ' | sed 's/?? /git add /' > add.sh
 chmod +x ./add.sh
+gedit add.sh
 ./add.sh
-svn remove ./add.sh --force
-svn remove ./tud.txt --force
+rm ./add.sh 
+#svn remove ./tud.txt --force
 # rm -f ./add.sh
-echo "Removing Deleted Files"
-svn export ./ ../paparazzi3/ --force
-cd ../paparazzi3/
-git status -s | grep 'TUDelft' | sed 's/??/rm -rf /' > rm.sh
-chmod +x ./rm.sh
-./rm.sh
-rm -rf ./import.sh
-rm -rf ./export.sh
-rm -rf ./sw/airborne/modules/onboardcam
-rm -rf ./sw/airborne/modules/opticflow
-rm -rf ./sw/airborne/boards/tiny_sense.h
-rm -rf ./sw/airborne/booz/aerovinci
-rm -rf ./conf/simulator/jsbsim/aircraft/LISA_BOOZ_BART.xml
-echo "Check this list manually: what files need to be added to ENAC server?" > ./rm.sh
-svn status | sed 's/?       /svn remove ..\/ppz2svn\//' >> rm.sh
-chmod +x ./rm.sh
-gedit ./rm.sh
+#echo "Removing Deleted Files"
+#svn export ./ ../paparazzi3/ --force
+#cd ../paparazzi3/
+#git status -s | grep 'TUDelft' | sed 's/??/rm -rf /' > rm.sh
+#chmod +x ./rm.sh
+#./rm.sh
+#rm -rf ./import.sh
+#rm -rf ./export.sh
+#rm -rf ./sw/airborne/modules/onboardcam
+#rm -rf ./sw/airborne/modules/opticflow
+#rm -rf ./sw/airborne/boards/tiny_sense.h
+#rm -rf ./sw/airborne/booz/aerovinci
+#rm -rf ./conf/simulator/jsbsim/aircraft/LISA_BOOZ_BART.xml
+#echo "Check this list manually: what files need to be added to ENAC server?" > ./rm.sh
+#svn status | sed 's/?       /svn remove ..\/ppz2svn\//' >> rm.sh
+#chmod +x ./rm.sh
+#gedit ./rm.sh
 #svn status | sed 's/?    /rm -rf /' > ./rm.sh
-cd ../ppz2svn
-svn diff > tud.txt
-gedit tud.txt
+#cd ../ppz2svn
+#svn diff > tud.txt
+#gedit tud.txt
